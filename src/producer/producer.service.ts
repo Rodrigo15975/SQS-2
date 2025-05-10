8a7536666ab3bb65dc01d73a28d7be81e5550896
rodrigo15975 <javierluis15975@hotmail.com>
2025-05-10 17:40:13 -0500

    update producer.service.ts

--- src/producer/producer.service.ts
+++ src/producer/producer.service.ts
@@ -1,41 +1,29 @@
 import { Injectable } from '@nestjs/common'
-import { CreateProducerDto } from './dto/create-producer.dto'
-import { UpdateProducerDto } from './dto/update-producer.dto'
-import { SQSClient, SendMessageCommand } from '@aws-sdk/client-sqs'
-
+import { SqsService } from '@ssut/nestjs-sqs'
+import { randomUUID } from 'node:crypto'
 @Injectable()
 export class ProducerService {
-  private readonly sqs = new SQSClient({ region: 'us-east-1' })
-  private readonly queueUrl =
-    'https://sqs.us-east-1.amazonaws.com/222634373780/mi-cola-prueba'
-  create(createProducerDto: CreateProducerDto) {
-    return 'This action adds a new producer'
-  }
+  // private readonly sqs = new SQSClient({ region: 'us-east-1' })
+  // private readonly queueUrl =
+  //   'https://sqs.us-east-1.amazonaws.com/222634373780/mi-cola-prueba'
+
+  constructor(private readonly sqsService: SqsService) {}
 
   async findAll() {
-    const comando = new SendMessageCommand({
-      QueueUrl: this.queueUrl,
-      MessageBody: JSON.stringify({
-        message: 'Data enviado es RODRIGO',
-      }),
-    })
+    const users = [
+      { id: 1, name: 'Juan' },
+      { id: 2, name: 'Maria' },
+      { id: 3, name: 'Carlos' },
+    ]
 
-    await this.sqs.send(comando)
-    console.log('✅ Mensaje enviado a SQS')
+    await this.sqsService.send('mi-cola-prueba', {
+      body: { message: 'Usuarios enviados', users },
+      id: randomUUID(),
+    })
+    console.log('Enviando a SQS')
+
     return {
       message: 'ok',
     }
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} producer`
-  }
-
-  update(id: number, updateProducerDto: UpdateProducerDto) {
-    return `This action updates a #${id} producer`
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} producer`
   }
 }
